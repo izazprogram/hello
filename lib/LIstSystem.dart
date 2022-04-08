0f0590cb918c8a47a15f8f825e3bfa79599d4ba8
izazul <izazul2016s@gmail.com>
2022-04-09 04:10:54 +0600

    listSystem listtile

--- lib/LIstSystem.dart
+++ lib/LIstSystem.dart
@@ -2,7 +2,9 @@
 import 'package:flutter/material.dart';
 
 class MyList extends StatelessWidget {
-  const MyList({Key? key}) : super(key: key);
+   MyList({Key? key}) : super(key: key);
+
+  List<String> names=["Bangla","English","Math","Pagli","Pagol",];
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +15,37 @@ class MyList extends StatelessWidget {
           style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
           backgroundColor: Colors.blueGrey,
         ),
-          body: ListView(
+          body: ListView.builder(
+
+            padding: EdgeInsets.all(30),
+            itemCount: names.length,
+              itemBuilder: (BuildContext context, int index) {
+              return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+                elevation: 3,
+                child: ListTile(
+                  autofocus: true,
+                  minLeadingWidth: 50.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+
+                  ),
+
+
+                  leading: FlutterLogo(size: 20,),
+                  trailing: Icon(Icons.delete),
+                  title: Text("Bangla"),
+                  subtitle: Text("English"),
+                  tileColor: Colors.lightGreen,
+                  isThreeLine: true,
+                ),
+              );
+              }
 
           ),
+
         )
     );
   }
